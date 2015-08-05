d9a50a0447f14caeb81ff243e759e91fde0f0c64
Gurpreet Singh <Guppst3r@gmail.com>
2015-08-05 16:59:47 -0400

    Started Java Fraction Adding

---
+++ easy/[#226] Adding Fractions/Java/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("How many fractions?");
+        int quantity = in.nextInt();
+
+        for (int i = 0; i < quantity; i++)
+        {
+
+        }
+    }
+}
