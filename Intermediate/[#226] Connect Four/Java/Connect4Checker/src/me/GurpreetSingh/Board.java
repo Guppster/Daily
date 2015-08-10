5f3e0a28c3a5b636fcef3176e83a5eaecd2b493e
Gurpreet Singh <Guppst3r@gmail.com>
2015-08-10 11:20:08 -0400

    Board class finished

--- Intermediate/[#226] Connect Four/Java/Connect4Checker/src/me/GurpreetSingh/Board.java
+++ Intermediate/[#226] Connect Four/Java/Connect4Checker/src/me/GurpreetSingh/Board.java
@@ -2,14 +2,32 @@ package me.GurpreetSingh;
 
 public class Board
 {
-    public Player[][] field;
+    private Player[][] field;       //Stores the playing field
+    private int[] columnHeight;     //Stores the highest lowest in each column of field
+
+    /**
+     * Le Constructor
+     */
     public Board(int x, int y)
     {
         field = new Player[x][y];
-    }
+        columnHeight = new int[y];
+    }//End of constructor
 
-    public Player getLeft(int x)
+    /**
+     * Gets the player at a specific location
+     */
+    public Player getPlayerAt(int col, int row)
     {
+        return field[col][row];
+    }//End of getPlayerAt
 
-    }
-}
+    /**
+     * Places a player in the specified column.
+     */
+    public void placePlayerInColumn(int x, Player player)
+    {
+        columnHeight[x]++;
+        field[x][columnHeight[x]] = player;
+    }//End of placePlayerInColumn
+}//End of board
--- Intermediate/[#226] Connect Four/Java/Connect4Checker/src/me/GurpreetSingh/Main.java
+++ Intermediate/[#226] Connect Four/Java/Connect4Checker/src/me/GurpreetSingh/Main.java
@@ -1,9 +1,17 @@
 package me.GurpreetSingh;
 
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        Scanner in = new Scanner(System.in);
 
+        System.out.println("Enter the width and height of the board");
+        Board board = new Board(in.nextInt(), in.nextInt());
+
+        System.out.println("\n Enter The Moves: ");
+        
     }//End of the main method
 }//End of the main class
