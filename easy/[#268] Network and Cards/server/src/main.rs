fd31ce7755272d28c5f0dbdb6cbb062a504eb0e3
Gurpreet Singh <Guppst3r@gmail.com>
2016-06-07 09:42:21 -0400

    basic server done

--- easy/[#268] Network and Cards/server/src/main.rs
+++ easy/[#268] Network and Cards/server/src/main.rs
@@ -4,5 +4,29 @@ use std::time::Duration;
 use std::thread;
 
 fn main() {
+	let listener = TcpListener::bind("localhost:8080")
+		.expect("Unable to obtain port 8080. Is the server already open?");
 
+	for stream in listener.incoming()
+	{
+		match stream{
+			Ok(s) => { thread::spawn(move|| send_heartbeats(s)); }
+			Err(e) => println! ("Connection failed {:?} , e);
+		}
+	}
+}
+
+fn send_heartbeats(mut s: TcpStream)
+{
+	let mut response = String::new();
+	let hb b"heart beat\n";
+	let mut buf_inp = BufReader::new(s.try_clone().unwrap());
+
+	loop{
+		s.write_all(hb).unwrap();
+		buf_inp.read_line(&mut response).unwrap();
+		println!("{}", response);
+		response.clear();
+		thread::sleep(Duration::from_secs(10));
+	{
 }
