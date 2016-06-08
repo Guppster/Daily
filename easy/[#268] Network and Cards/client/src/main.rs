0ff708c6ba18783ea043e637d7f0f1b9d843f6a7
Gurpreet Singh <Guppst3r@gmail.com>
2016-06-08 09:49:49 -0400

    basic client finished

--- easy/[#268] Network and Cards/client/src/main.rs
+++ easy/[#268] Network and Cards/client/src/main.rs
@@ -1,3 +1,25 @@
+use std::net::TcpStream;
+use std::io::{Write, BufRead, BufReader};
+
+
 fn main() {
-    println!("Hello, world!");
+	let server = std::env::args().skip(1).next()
+		.expect("Pass a server address as an argument");
+	
+	let s = TcpStream::connect(&*server).expect("Could not reach server.");
+	handle_heartbeats(s);
+}
+
+fn handle_heartbeats(mut s: TcpStream)
+{
+	let mut hb = String::new();
+	let mut read = BufReader::new(s.try_clone().unwrap());
+
+	loop
+	{
+		read.read_line(&mut hb).unwrap();
+		s.write_all(hb.as_bytes()).unwrap();
+		println!("{}", hb);
+		hb.clear();
+	}
 }
