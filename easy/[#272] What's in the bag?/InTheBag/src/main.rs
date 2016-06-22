bbe12055fafeba68f3a70d55ef9ca58221c00d5d
Gurpreet Singh <Guppst3r@gmail.com>
2016-06-22 13:45:08 -0400

    idk whats going on

--- easy/[#272] What's in the bag?/InTheBag/src/main.rs
+++ easy/[#272] What's in the bag?/InTheBag/src/main.rs
@@ -6,4 +6,19 @@ fn main() {
                     'Q' => 1, 'R' => 6, 'S' => 4, 'T' => 6, 'U' => 4, 'V' => 2, 'W' => 2, 'X' => 1,
                     'Y' => 2, 'Z' => 1,  '_' => 2};
     let arg = std::env::args().nth(1).expect("You must provide which letters have been drawn");
+    for c in arg.chars(){
+        let counter = m.get_mut(&c).expect(&format!("'{}' is not a valid character",c)); *counter-=1;
+        if *counter<0{
+            println!("Invalid Input. More {}'s have been taken from the bag than possible.",c);
+            return;
+        }
+    }
+    let mut m2 = std::collections::BTreeMap::new();
+    for (&key,&value) in m.iter(){
+        m2.entry(value).or_insert(Vec::new()).push(key.to_string());
+    }
+    for (key,value) in m2.iter_mut().rev(){
+        value.sort();
+        println!("{}: {}",key,value.join(", "));
+    }
 }
