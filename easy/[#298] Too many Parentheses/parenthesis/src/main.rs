05a469e9e6df8390c26f813963e38ea2af97942c
Gurpreet Singh <gsingh@ca.ibm.com>
2017-02-23 11:06:59 -0500

    wrote tests

--- easy/[#298] Too many Parentheses/parenthesis/src/main.rs
+++ easy/[#298] Too many Parentheses/parenthesis/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#[cfg(test)]
+mod tests 
+{
+    use super::{bracket_map, solve};
+
+    #[test]
+    fn bracket_map_test() 
+    {
+        assert_eq!(bracket_map("(ab(c))"), vec![(3usize, 5usize), (0usize, 6usize)]);
+    }
+
+    #[test]
+    fn solve_test() 
+    {
+        assert_eq!(&solve("()"), "NULL");
+        assert_eq!(&solve("()(abc())"), "(abc)");
+        assert_eq!(&solve("((a((bc)(de)))f)"), "((a((bc)(de)))f)");
+    }
 }
